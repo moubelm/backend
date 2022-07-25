68f8911d44ae5527d6c0aaa9b0d2d769ae1dbdf7
moubelm <moubelm@gmail.com>
2022-07-25 17:05:50 +0200

    first commit

--- mix.exs
+++ mix.exs
@@ -11,6 +11,13 @@ defmodule Backend.MixProject do
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test
+      ]
 
     ]
   end
